f357bcd83037ca10c37bc680a2b8c87b91f93e17
bruno159 <brunoleao.si@gmail.com>
2018-03-22 12:37:30 -0300

    Alterações certas

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
-<<<<<<< HEAD
+
 	echo "Olá Mundo Legal!";
 
-	echo "Vida Longa!";
-=======
-	echo "Olá Mundo Pevertido!";
+	echo "Universum Dev";
 
-	echo "Olá UniversumDev!";
->>>>>>> 08f894ce9d85715846dd1d00da717a430495e837
 
 ?>
